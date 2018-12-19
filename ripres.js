7803c6b4c275a7a7198c49448eaae0adad477d4e
sancan25 <43161960+sancan25@users.noreply.github.com>
2018-12-19 11:10:53 +0800

    Update ripres.js

--- ripres.js
+++ ripres.js
@@ -139,7 +139,8 @@ if(x=="3"){xxx="/2018/12/the-clauses-of-insurance-policy.html"}
 if(x=="4"){xxx="/2018/12/all-about-personal-accident-insurance.html"}
 if(x=="5"){xxx="/2018/12/importance-and-benefits-of-travel.html"}
 if(x=="6"){xxx="/2018/12/5-important-types-of-travel-insurance.html"}
-if(x=="7"){xxx="/2018/12/4-types-of-insurance-and-its-importance.html"}a_to_ve[i].href="https://wingsinsurance.blogspot.com"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="7"){xxx="/2018/12/4-types-of-insurance-and-its-importance.html"}
+if(x=="8"){xxx="/2018/12/home-insurance-tips-for-large-families.html"}a_to_ve[i].href="https://wingsinsurance.blogspot.com"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
     var a_to_vj = document.getElementById("anonyminized");
     var a_to_vk = document.getElementById("found_links");
     if (a_to_vj) {
